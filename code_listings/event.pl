9ace826bd4db0ee5e58ffedb14c0b589a990ed02
kthakore <thakore.kartik@gmail.com>
2010-09-25 14:45:01 -0400

    Clean up and Refactor

--- code_listings/event.pl
+++ code_listings/event.pl
@@ -2,24 +2,15 @@ use strict;
 use warnings;
 use SDL;
 use SDL::Event;
-use SDL::Events;
-
 use SDLx::App;
 
-my $app = SDLx::App->new( w => 200, h => 200, d => 32 );
-
+my $app = SDLx::App->new( w => 200, h => 200, d => 32, title => "Quit Events");
 $app->add_event_handler( \&quit_event );
-
 $app->run();
 
+sub quit_event {
 
-
-sub quit_event
-{
-
-	my $event = shift;
-
-	return 0 if $event->type == SDL_QUIT;
-
-	return 1;
+    my $event = shift;
+    return 0 if $event->type == SDL_QUIT;
+    return 1;
 }
