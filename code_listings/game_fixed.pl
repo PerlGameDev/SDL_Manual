4591cfe4e8098720b7f233a1f866d80e2ab366a2
kthakore <thakore.kartik@gmail.com>
2010-10-08 21:56:56 -0400

    Finished Fixed FPS section

--- code_listings/game_fixed.pl
+++ code_listings/game_fixed.pl
@@ -7,10 +7,10 @@ use SDL::Events;
 use SDLx::App;
 
 my $app = SDLx::App->new(
-		width  => 200,
-		height => 200,
-		title  => 'Pew Pew'
-		);
+    width  => 200,
+    height => 200,
+    title  => 'Pew Pew'
+);
 
 my ( $start, $end, $delta_time, $FPS, $frames ) = ( 0, 0, 0, 0, 0 );
 
@@ -22,85 +22,77 @@ my $lazer = 0;
 
 sub get_events {
 
-	my $event = SDL::Event->new();
+    my $event = SDL::Event->new();
 
-#Pump the event queue
-	SDL::Events::pump_events;
+    #Pump the event queue
+    SDL::Events::pump_events;
 
-	while ( SDL::Events::poll_event($event) ) {
-		$quit = 1 if $event->type == SDL_QUIT;
-	}
+    while ( SDL::Events::poll_event($event) ) {
+        $quit = 1 if $event->type == SDL_QUIT;
+    }
 }
 
 sub calculate_next_positions {
-	$lazer++;
+    $lazer++;
 
-	$lazer = 0 if $lazer > $app->w;
+    $lazer = 0 if $lazer > $app->w;
 }
 
 sub render {
 
-#Draw the background first
-	$app->draw_rect( [ 0, 0, $app->w, $app->h ], 0 );
+    #Draw the background first
+    $app->draw_rect( [ 0, 0, $app->w, $app->h ], 0 );
 
-#Draw the lazer
-	$app->draw_rect( [ $lazer, $app->h / 2, 10, 2 ], [ 255, 0, 0, 255 ] );
+    #Draw the lazer
+    $app->draw_rect( [ $lazer, $app->h / 2, 10, 2 ], [ 255, 0, 0, 255 ] );
 
-	$app->draw_gfx_text( [ 10, 10 ], [ 255, 0, 255, 255 ], "FPS: $FPS" );
+    $app->draw_gfx_text( [ 10, 10 ], [ 255, 0, 255, 255 ], "FPS: $FPS" );
 
-	$app->update();
+    $app->update();
 }
 
-my $fps_check = (1000/60);
+my $fps_check = ( 1000 / 60 );
 
-sub calculate_fps_at_frame_end
-{
+sub calculate_fps_at_frame_end {
 
-	$end = SDL::get_ticks();
+    $end = SDL::get_ticks();
 
-	if ( $frames < 10 ) {
-		$frames++;
-		$delta_time += $end - $start;
-	}
-	else {
-		$FPS        = int( ( $frames * 100 ) / $delta_time );
-		$frames     = 0;
-		$delta_time = 0;
-	}
-
-
+    if ( $frames < 10 ) {
+        $frames++;
+        $delta_time += $end - $start;
+    }
+    else {
+        $FPS        = int( ( $frames * 100 ) / $delta_time );
+        $frames     = 0;
+        $delta_time = 0;
+    }
 
 }
 
 while ( !$quit ) {
 
+    $start = SDL::get_ticks();
 
-	$start = SDL::get_ticks();
+    get_events();
 
-	get_events();
+    if ( $ARGV[1] ) {
+        if ( $delta_time > $fps_check ) {
 
-	if( $ARGV[1] )
-	{
-		if ( $delta_time > $fps_check ) {
+            calculate_fps_at_frame_end();
+            next;
 
-			calculate_fps_at_frame_end();
-			next;		  
+        }
 
-		}
+    }
+    calculate_next_positions();
+    render();
 
-	}
-	calculate_next_positions();
-	render();
+    calculate_fps_at_frame_end();
 
-
-	calculate_fps_at_frame_end();
-
-
-	if ( $ARGV[0] ) {
-		if ( $delta_time < $fps_check ) {
-			SDL::delay( $fps_check - $delta_time );
-		}
-	}
-
+    if ( $ARGV[0] ) {
+        if ( $delta_time < $fps_check ) {
+            SDL::delay( $fps_check - $delta_time );
+        }
+    }
 
 }
---
+++ code_listings/game_loop_simple.pl
@@ -0,0 +1,55 @@
+
+    use strict;
+    use warnings;
+    use SDL;
+    use SDL::Event;
+    use SDL::Events;
+    use SDLx::App;
+
+    my $app = SDLx::App->new(
+        width  => 200,
+        height => 200,
+        title  => 'Pew Pew'
+    );
+
+    #Don't need to quit yet
+    my $quit = 0;
+
+    #Start lazer on the left
+    my $lazer = 0;
+
+    sub get_events {
+
+        my $event = SDL::Event->new();
+
+        #Pump the event queue
+        SDL::Events::pump_events;
+
+        while ( SDL::Events::poll_event($event) ) {
+            $quit = 1 if $event->type == SDL_QUIT;
+        }
+    }
+
+    sub calculate_next_positions {
+        $lazer++;
+        $lazer = 0 if $lazer > $app->w();
+    }
+
+    sub render {
+
+        #Draw the background first
+        $app->draw_rect( [ 0, 0, $app->w, $app->h ], 0 );
+
+        #Draw the lazer
+        $app->draw_rect( [ $lazer, $app->h / 2, 10, 2 ], [ 255, 0, 0, 255 ] );
+
+        $app->update();
+
+    }
+
+    while ( !$quit ) {
+        get_events();
+        calculate_next_positions();
+        render();
+    }
+
