cbe578b56a3c85640e44f30a3298973572c90bf6
kthakore <thakore.kartik@gmail.com>
2010-12-24 11:46:39 -0500

    Finished playing samples

--- code_listings/music_sound.pl
+++ code_listings/music_sound.pl
@@ -4,7 +4,8 @@ use SDL;
 use Carp;
 use SDL::Audio;
 use SDL::Mixer;
-
+use SDL::Mixer::Samples;
+use SDL::Mixer::Channels;
 SDL::init(SDL_INIT_AUDIO);
 
 unless( SDL::Mixer::open_audio( 44100, AUDIO_S16SYS, 2, 4096 ) == 0 )
@@ -12,4 +13,15 @@ unless( SDL::Mixer::open_audio( 44100, AUDIO_S16SYS, 2, 4096 ) == 0 )
 	Carp::croak "Cannot open audio: ".SDL::get_error(); 
 }
 
+
+my $sample = SDL::Mixer::Samples::load_WAV('data/sample.wav');
+
+unless( $sample)
+{
+	Carp::croak "Cannot load file data/sample.wav: ".SDL::get_error(); 
+}
+
+my $playing_channel = SDL::Mixer::Channels::play_channel( -1, $sample, 1 );
+
+sleep(2);
 SDL::Mixer::close_audio;
