fffeb8f7c178325b330c430d07c50531a7af7ca2
kthakore <thakore.kartik@gmail.com>
2010-12-24 10:54:24 -0500

    Using sound

---
+++ code_listings/music_sound.pl
@@ -0,0 +1,13 @@
+use strict;
+use warnings;
+use SDL;
+use Carp;
+use SDL::Audio;
+use SDL::Mixer;
+
+use SDL::init(SDL_INIT_AUDIO);
+
+unless( SDL::Mixer::open_audio( 44100, AUDIO_S16SYS, 2, 4096 ) == 0 )
+{
+	Carp::croak "Cannot open audio: ".SDL::get_error(); 
+}
