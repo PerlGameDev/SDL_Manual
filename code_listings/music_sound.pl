7c377add5473616c9f76b9d0f150f791f4fdd087
kthakore <thakore.kartik@gmail.com>
2010-12-24 10:55:13 -0500

    Fixed typo

--- code_listings/music_sound.pl
+++ code_listings/music_sound.pl
@@ -5,9 +5,11 @@ use Carp;
 use SDL::Audio;
 use SDL::Mixer;
 
-use SDL::init(SDL_INIT_AUDIO);
+SDL::init(SDL_INIT_AUDIO);
 
 unless( SDL::Mixer::open_audio( 44100, AUDIO_S16SYS, 2, 4096 ) == 0 )
 {
 	Carp::croak "Cannot open audio: ".SDL::get_error(); 
 }
+
+SDL::Mixer::close_audio;
