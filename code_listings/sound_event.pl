3c2b773230edde4e73f5b891d625e6c89b63851d
kthakore <thakore.kartik@gmail.com>
2010-12-24 15:41:52 -0500

    Background Music with play, pause and resume + Volume control

--- code_listings/sound_event.pl
+++ code_listings/sound_event.pl
@@ -23,6 +23,9 @@ unless( SDL::Mixer::open_audio( 44100, AUDIO_S16SYS, 2, 4096 ) == 0 )
 	Carp::croak "Cannot open audio: ".SDL::get_error(); 
 }
 
+my $channel_volume = 100;
+my $music_volume = 100;
+
 # Load our sound resources
 my $laser = SDL::Mixer::Samples::load_WAV('data/sample.wav');
 unless( $laser)
@@ -31,7 +34,6 @@ unless( $laser)
 }
 
 my $background_music = SDL::Mixer::Music::load_MUS('data/music/01-PC-Speaker-Sorrow.ogg');
-
 unless( $background_music )
 {
 	Carp::croak "Cannot load music: ".SDL::get_error() ;
@@ -46,12 +48,55 @@ my $event = shift;
 		my $keysym = $event->key_sym; 
 		my $keyname = SDL::Events::get_key_name( $keysym );
 
-		if( $keyname =~ 'space' )
+		if( $keyname eq 'space' )
 		{
 			#fire lasers!
 			SDL::Mixer::Channels::play_channel( -1, $laser, 0 );
 		
 		}
+		elsif ( $keyname eq 'up' )
+		{
+			$channel_volume += 5 unless $channel_volume == 100;	
+		}
+		elsif ( $keyname eq 'down' )
+		{
+			$channel_volume -= 5 unless $channel_volume == 0;
+		}
+		elsif ( $keyname eq 'right' )
+		{
+			$music_volume += 5 unless $music_volume == 100;	
+		}
+		elsif ( $keyname eq 'left' )
+		{
+			$music_volume -= 5 unless $music_volume == 0;
+		}
+		elsif ( $keyname eq 'return' )
+		{
+			my $playing = SDL::Mixer::Music::playing_music();
+			my $paused = SDL::Mixer::Music::paused_music();
+			
+			warn "Playing $playing and Paused $paused";
+			if( $playing == 0 && $paused == 0)
+			{
+				SDL::Mixer::Music::play_music( $background_music, 1 );
+			}
+			elsif ( $playing && !$paused )
+			{
+				SDL::Mixer::Music::pause_music( );
+			}
+			elsif ( $playing && $paused )
+			{
+				SDL::Mixer::Music::resume_music( );
+			}
+
+		}
+
+		warn "Channel Volume is $channel_volume";
+		SDL::Mixer::Channels::volume(-1, $channel_volume);
+
+		warn "Channel Volume is $music_volume";
+		SDL::Mixer::Music::volume_music($music_volume);
+
 	}
 
 
@@ -60,3 +105,7 @@ my $event = shift;
 );
 
 $app->run();
+
+SDL::Mixer::Music::halt_music();
+SDL::Mixer::close_audio;
+
