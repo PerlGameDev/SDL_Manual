95dfad3d320236e826265da1128418ef22ef4bf6
kthakore <thakore.kartik@gmail.com>
2010-12-24 13:07:14 -0500

    Using a lazer sound sample. Also finished simple music playback

--- code_listings/music_sound.pl
+++ code_listings/music_sound.pl
@@ -6,6 +6,7 @@ use SDL::Audio;
 use SDL::Mixer;
 use SDL::Mixer::Samples;
 use SDL::Mixer::Channels;
+use SDL::Mixer::Music;
 SDL::init(SDL_INIT_AUDIO);
 
 unless( SDL::Mixer::open_audio( 44100, AUDIO_S16SYS, 2, 4096 ) == 0 )
@@ -21,7 +22,19 @@ unless( $sample)
 	Carp::croak "Cannot load file data/sample.wav: ".SDL::get_error(); 
 }
 
-my $playing_channel = SDL::Mixer::Channels::play_channel( -1, $sample, 1 );
+my $playing_channel = SDL::Mixer::Channels::play_channel( -1, $sample, 0 );
+
+#Load our awesome music from U<http://8bitcollective.com>
+my $background_music = SDL::Mixer::Music::load_MUS('data/music/01-PC-Speaker-Sorrow.ogg');
+
+unless( $background_music )
+{
+	Carp::croak "Cannot load music file data/music/01-PC-Speaker-Sorrow.ogg: ".SDL::get_error() ;
+}
+
+SDL::Mixer::Music::play_music( $background_music,0 );
 
 sleep(2);
+
+SDL::Mixer::Music::halt_music();
 SDL::Mixer::close_audio;
