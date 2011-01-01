d55ec084e1e3ad61992ec2144bafda7cef5f3265
kthakore <thakore.kartik@gmail.com>
2011-01-01 02:13:37 -0500

    Clean up and bumped the script

--- code_listings/stereograph.pl
+++ code_listings/stereograph.pl
@@ -18,22 +18,16 @@ use SDL::Mixer::Music;
 use SDL::Mixer::Effects;
 
 use SDLx::App;
-
-my $stream_data :shared = '';
-my $stream_lock :shared = 0;
-
-
-my $lines = 100;
-
 my $app = SDLx::App->new(
     init   => SDL_INIT_AUDIO | SDL_INIT_VIDEO,
 	width  => 800,
 	height => 600,
 	depth  => 32,
-    title  => "Sound Event Demo",
+    title  => "Music Visualizer",
     eoq    => 1,
 	dt     => 0.2,
 );
+
 
 # Initialize the Audio
 unless ( SDL::Mixer::open_audio( 44100, AUDIO_S16, 2, 1024 ) == 0 ) {
@@ -43,6 +37,9 @@ unless ( SDL::Mixer::open_audio( 44100, AUDIO_S16, 2, 1024 ) == 0 ) {
 # Load our music files
 my $data_dir = '.';
 my @songs = glob 'data/music/*.ogg';
+
+my $stream_data :shared = '';
+my $stream_lock :shared = 0;
 
 # Music Effect to pull Stream Data 
 sub music_data {
@@ -60,8 +57,19 @@ sub music_data {
 }
 
 sub done_music_data { }
+
+my $music_data_effect_id = 
+      SDL::Mixer::Effects::register( MIX_CHANNEL_POST, "main::music_data",
+        "main::done_music_data", 0 );
+
+
 # Music Playing Callbacks 
 my $current_song = 0;
+
+
+my $lines = $ARGV[0];
+   $lines = 50 unless $lines;
+
 
 my $current_music_callback = sub { 
 	my( $delta, $app ) = @_;
@@ -70,29 +78,37 @@ my $current_music_callback = sub {
 	if( $stream_lock == 0  ){
 
 	$app->draw_rect([ 0, 0, $app->w(), $app->h()], 0x000000FF );
+	$app->draw_gfx_text([5,$app->h()-10], [255,0,0,255], "Playing Song: ".$songs[$current_song-1]);
 
             my @stream = split( ',', $stream_data );
             $stream_data = '';
 			my @left;
             my @right;
+
+			#To show the right amount of lines we choose a cut of the stream
+			#this is purely for asthetic reasons. 
             my $cut =  $#stream/$lines;
             my @x;
-                       
+            
+			#The width of each line is calculated to use.           
             my $l_wdt= ( $app->w() / $lines) /2;
             
             
             for ( my $i = 0 ; $i < $#stream ; $i += $cut ) {
 
+			   # In stereo mode the stream is split between two alternating streams
                 my $left  = $stream[$i];
                 my $right = $stream[ $i + 1 ];
 
+				# For each bar we calculate a Y point and a X point 
                 my $point_y   = ( ( ($left) ) * $app->h()/4 / 32000 ) + ($app->h/2);
                 my $point_y_r = ( ( ($right) ) * $app->h()/4 / 32000 )+ ($app->h/2);
                 my $point_x   = ( $i / $#stream ) * $app->w;
                 
-           
-                SDL::GFX::Primitives::box_RGBA(  $app, $point_x-$l_wdt, 300, $point_x+$l_wdt, $point_y, 40, 0, 255, 128 );
-                SDL::GFX::Primitives::box_RGBA(  $app, $point_x-$l_wdt, 300, $point_x+$l_wdt, $point_y_r , 255, 0, 40, 128 );
+           		#Using the parameters  
+				#  Surface, box coordinates and color as RGBA
+                SDL::GFX::Primitives::box_RGBA(  $app, $point_x-$l_wdt, $app->h()/2, $point_x+$l_wdt, $point_y, 40, 0, 255, 128 );
+                SDL::GFX::Primitives::box_RGBA(  $app, $point_x-$l_wdt, $app->h()/2, $point_x+$l_wdt, $point_y_r , 255, 0, 40, 128 );
 
             }
         
@@ -102,8 +118,8 @@ my $current_music_callback = sub {
 	$app->flip();
 };
 
-my $cms_move_callback;
-my $pns_move_callback; 
+my $cms_move_callback_id;
+my $pns_move_callback_id; 
 
 my $play_next_song_callback = sub {
 
@@ -113,20 +129,20 @@ my $play_next_song_callback = sub {
 	SDL::Mixer::Music::hook_music_finished('main::music_finished_playing');
 	SDL::Mixer::Music::play_music($song, 0 );
 
-	$app->remove_move_handler( $pns_move_callback ) if defined $pns_move_callback;
-	$cms_move_callback = $app->add_show_handler( $current_music_callback );
+	$app->remove_move_handler( $pns_move_callback_id ) if defined $pns_move_callback_id;
+	$cms_move_callback_id = $app->add_show_handler( $current_music_callback );
 };
 
 sub music_finished_playing { 
 
 	SDL::Mixer::Music::halt_music();
-	$pns_move_callback = $app->add_move_handler( $play_next_song_callback ); 
-	$app->remove_show_handler($cms_move_callback); 
+	$pns_move_callback_id = $app->add_move_handler( $play_next_song_callback ); 
+	$app->remove_show_handler($cms_move_callback_id); 
 
 }
-my $music_data_effect_id = 
-      SDL::Mixer::Effects::register( MIX_CHANNEL_POST, "main::music_data",
-        "main::done_music_data", 0 );
+
+$pns_move_callback_id= $app->add_move_handler( $play_next_song_callback);
+
 
 $app->add_event_handler(
 sub {
@@ -140,9 +156,6 @@ sub {
 
 }
 );
-
-$pns_move_callback= $app->add_move_handler( $play_next_song_callback);
-
 
 $app->run();
 
